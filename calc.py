455cb91e0ccdc0de5d619a1aae466389b14db789
abeshansky <abeshansky1@babson.edu>
2016-09-03 15:02:53 -0400

    calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,14 @@
 print (46 + 37 + 38)
 print (2016-1996)
 print (15/3)
-print (2**6))
+print (2**6)
+
+print ("Exercise 2")
+print ("1")
+print (42*60+42)
+print ("2")
+print (10/1.61)
+print ("3")
+print (42+42/60/(10*1.61) "minutes per mile")
+print (42*60+42/(10*1.61) "seconds per mile")
+print (10*1.61/((42+(42/60))/60)) "miles per hour"
