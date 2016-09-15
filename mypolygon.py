2b501e7fa2af3888eb37feaa6f0061a181a728ef
abeshansky <abeshansky1@babson.edu>
2016-09-15 16:43:46 -0400

    mypolygon.py

---
+++ mypolygon.py
@@ -0,0 +1,34 @@
+import turtle
+
+def square(t, length):
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+def polygon(t, length, n):
+    for i in range(n):
+        t.fd(length)
+        t.lt(360/n)
+
+jude = turtle.Turtle()
+polygon(jude, 150, 4)
+
+jerry = turtle.Turtle()
+square(jerry, 100)
+
+teresa = turtle.Turtle()
+square(teresa, 250)
+
+#polygon(alex, n=7, length=70)
+#polygon(alex, 70, 7)
+#these are the same thing - keyword arguments
+
+import math
+def circle(t, r):
+    circumference = 2 * math.pi * r
+    n = 50
+    length = circumference/n
+    polygon(t, n, length)
+
+chris = turtle.Turtle()
+circle(chris, 30)
